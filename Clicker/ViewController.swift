60505870b0e36f6a6cc59bf12adf314b2d92346b
Eli <eli@republic.co>
2019-01-21 07:23:26 -0500

    Initial Commit

---
+++ Clicker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Clicker
+//
+//  Created by Eli on 1/21/19.
+//  Copyright © 2019 Eli. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
